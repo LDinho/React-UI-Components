e69db218d85412480b24c2bbe25e23adcf988399
LD <LDinho@users.noreply.github.com>
2019-02-28 00:56:21 -0500

    update react version & add Header component w/ styling

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle() {
+  return (
+    <>
+      <h2>
+        Lambda School
+        <span className="grayed-out">@LambdaSchool 26 jan</span>
+      </h2>
+    </>
+  )
+}
+
+export default HeaderTitle;
