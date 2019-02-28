b988eada213649e115964b2a1ee76f3bf2129458
LD <LDinho@users.noreply.github.com>
2019-02-28 03:13:38 -0500

    MVP-add card container component w/ styling

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,25 @@
 import React from 'react';
 import './Card.css';
+
+import CardBanner from './CardBanner';
+import CardContent from './CardContent';
+
+function CardContainer() {
+  return (
+    <a className="decoration" href="https://www.reactjs.org" target="#">
+      <div className="card-container">
+        <div className="card-wrapper">
+          <div className="banner-wrapper">
+            <CardBanner />
+          </div>
+
+          <div className="card-content-wrapper">
+            <CardContent />
+          </div>
+        </div>
+      </div>
+    </a>
+  )
+}
+
+export default CardContainer;
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,25 @@
 import React from 'react';
 import './Card.css';
+
+function CardContent() {
+  return (
+    <>
+      <h2>Get started with React</h2>
+      <p className="card-content">
+        React makes it painless to
+        create interactive UIs.
+        Design simple views for
+        each state in your application.
+      </p>
+
+        <a className="decoration text-color-light" href="https://www.reactjs.org"
+            target="#">
+          <p className="card-content">reactjs.org</p>
+        </a>
+
+    </>
+  )
+}
+
+export default CardContent;
+
